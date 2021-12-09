6186fa6b3c4194f2cde6e62439b7fec480f2c41c
Akremibrahim <akrem.ibrahim@gmail.comgit config --global user.name Akrem>
2021-12-09 04:19:00 +0000

    completed snake client

--- play.js
+++ play.js
@@ -1,10 +1,21 @@
 const net = require("net");
 
+//!/usr/bin/env node
+
+process.stdout.write('\x07');
+
+const { Game } = require('./src/Game')
+const { UserInterface } = require('./src/UserInterface')
+const { RemoteInterface } = require('./src/RemoteInterface')
+const game = new Game(new UserInterface(), new RemoteInterface())
+
+// Begin game
+game.start()
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host:10.0.2.15  // IP address here,
-    port:50541  // PORT number here,
+    host: '192.168.2.14',// IP address here,
+    port: "50541",// PORT number here,
   });
 
   // interpret incoming data as text
